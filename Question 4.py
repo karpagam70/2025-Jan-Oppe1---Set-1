f58076f4478d9f7b8400011b8a58dab233539366
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 18:34:53 +0530

    Create Question 4.py

---
+++ Question 4.py
@@ -0,0 +1,16 @@
+'''
+Counts unique even and odd numbers
+Write a function count_unique_even_odd that takes list of integers, returns a dictionary with the count of unique even and odd numbers in the list.
+
+NOTE: This is a function type question, you don't have to take input or print the output, just have to complete the required function definition.
+
+Example
+
+>>> l = [1, 2, 2, 3, 4, 5, 5, 6, 9]
+>>> count_unique_even_odd(l)
+{"even": 3, "odd": 4}
+Explanation
+
+unique even numbers are 2,4 and 6 and their count is 3
+unique odd numbers are 1,3,5 and 9 and their count is 4
+'''
